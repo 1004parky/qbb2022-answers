e013c7812aa870bbe295536c9c21be718a3e1afb
Christina <1004parky@gmail.com>
2022-08-30 17:53:14 -0400

    Exercise 2

--- day2-homework/README.md
+++ day2-homework/README.md
@@ -1,2 +1,12 @@
 # QBB2022 - Day 2 - Homework Excercises Submission
 
+## Exercise 2
+Output from running my script:
+```
+There were 24 malformed entries
+101 records did not have matching IDs in second file.
+```
+So there were 101 records.
+
+I had implemented a check when creating the position to ID dictionary that checked to make sure the IDs were not overwritten. I learned that the same pos can have different IDs based on the reference allele and/or the alternate allele, so I decided to change the code to create a mapping between (pos, ref, alt) and the ID.
+
--- day2-homework/vcfParser.py
+++ day2-homework/vcfParser.py
@@ -29,96 +29,98 @@ def parse_vcf(fname):
   except:
     raise FileNotFoundError(f"{fname} does not appear to exist", file=sys.stderr)
 
-  for line in f: 
-    if line.startswith("#"):
-      # Parse header lines; contain valuable information
-      try:
-        if line.startswith("##FORMAT"):
-          # parse format info line, associate description with ID
-          fields = line.split("=<")[1].rstrip(">\r\n") + ","
-          i = 0
-          start = 0
-          in_string = False
-          while i < len(fields):
-            if fields[i] == "," and not in_string:
-              name, value = fields[start:i].split('=')
-              if name == "ID":
-                ID = value
-              elif name == "Description":
-                desc = value
-              start = i + 1
-            elif fields[i] == '"':
-              in_string = not in_string
-            i += 1
-          format_description[ID] = desc.strip('"')
-        elif line.startswith("##INFO"):
-          # parse info info line, associate description with ID
-          fields = line.split("=<")[1].rstrip(">\r\n") + ","
-          i = 0
-          start = 0
-          in_string = False
-          while i < len(fields):
-            if fields[i] == "," and not in_string:
-              name, value = fields[start:i].split('=')
-              if name == "ID":
-                  ID = value
-              elif name == "Description":
-                  desc = value
-              elif name == "Type":
-                  Type = value
-              start = i + 1
-            elif fields[i] == '"':
-              in_string = not in_string
-            i += 1
-          info_description[ID] = desc.strip('"')
-          info_type[ID] = Type
-        elif line.startswith('#CHROM'):
-          # parse column line. 
-          fields = line.lstrip("#").rstrip().split("\t")
+  for line in f:
+      if line.startswith("#"):
+        try:
+          # Parse header lines; contain valuable information
+          if line.startswith("##FORMAT"):
+            fields = line.split("=<")[1].rstrip(">\r\n") + ","
+            i = 0
+            start = 0
+            in_string = False
+            while i < len(fields):
+              if fields[i] == "," and not in_string:
+                if fields[start:i].count("=") == 1:
+                  name, value = fields[start:i].split('=')
+                  if name == "ID":
+                    ID = value
+                  elif name == "Description":
+                    desc = value
+                start = i + 1
+              elif fields[i] == '"':
+                in_string = not in_string
+              i += 1
+            format_description[ID] = desc.strip('"')
+          elif line.startswith("##INFO"):
+            # parse info info line, associate description with ID
+            fields = line.split("=<")[1].rstrip(">\r\n") + ","
+            i = 0
+            start = 0
+            in_string = False
+            while i < len(fields):
+              if fields[i] == "," and not in_string:
+                if fields[start:i].count("=") == 1:
+                  name, value = fields[start:i].split('=')
+                  if name == "ID":
+                    ID = value
+                  elif name == "Description":
+                    desc = value  
+                  elif name == "Type":
+                    Type = value  
+                start = i + 1
+              elif fields[i] == '"':
+                in_string = not in_string
+              i += 1
+            info_description[ID] = desc.strip('"')
+            info_type[ID] = Type
+          elif line.startswith('#CHROM'):
+            # parse column line. 
+            fields = line.lstrip("#").rstrip().split("\t")
+            vcf.append(fields)
+        except:
+          raise RuntimeError("Malformed header")
+      else:
+        # Data lines
+        # 8 fixed fields
+        # CHROM | POS | ID | REF | ALT | QUAL | FILTER | INFO | (other genotype fields)
+        # all rows must have the same columns
+        try:
+          fields = line.rstrip().split("\t")
+          fields[1] = int(fields[1]) # pos
+          if fields[5] != ".":
+            fields[5] = float(fields[5]) # qual 
+          info = {}
+          for entry in fields[7].split(";"): # info 
+            # for every data value in info 
+            temp = entry.split("=")
+            if len(temp) == 1:
+              info[temp[0]] = None
+            else:
+              name, value = temp
+              Type = info_type[name] # capitalize Type since type is a keyword..
+              info[name] = type_map[Type](value)
+          fields[7] = info # info is dict
+          if len(fields) > 8: # format column precedes any genotype data columns
+            fields[8] = fields[8].split(":")
+            if len(fields[8]) > 1:
+                for i in range(9, len(fields)):
+                  # All the genotype data
+                  fields[i] = fields[i].split(':')
+            else:
+              # If only one genotype column, just store it as the string, not list 
+              fields[8] = fields[8][0]
           vcf.append(fields)
-      except:
-        raise RuntimeError("Malformed header")
-    else:
-      # Data lines
-      # 8 fixed fields
-      # CHROM | POS | ID | REF | ALT | QUAL | FILTER | INFO | (other genotype fields)
-      # all rows must have the same columns
-      try:
-        fields = line.rstrip().split("\t")
-        fields[1] = int(fields[1]) # pos
-        if fields[5] != ".":
-          fields[5] = float(fields[5]) # qual 
-        info = {}
-        for entry in fields[7].split(";"): # info 
-          # for every data value in info
-          temp = entry.split("=")
-          if len(temp) == 1:
-            info[temp[0]] = None
-          else:
-            name, value = temp
-            Type = info_type[name] # capitalize Type since type is a keyword.. 
-            info[name] = type_map[Type](value)
-        fields[7] = info # dict
-        if len(fields) > 8: # genotype fields
-          fields[8] = fields[8].split(":")
-          if len(fields[8]) > 1:
-            for i in range(9, len(fields)):
-              fields[i] = fields[i].split(':') 
-          else:
-            fields[8] = fields[8][0]
-        vcf.append(fields)
-      except:
-        # Keep track of how many malformed lines
-        malformed += 1
+        except:
+          # Keep track of how many malformed lines
+          malformed += 1
 
-    # after parsing file, set 8th element of 1st parsed row as info_description... 
-    vcf[0][7] = info_description
-    # If there is space, set 9th element of 1st parsed row as format_description... 
-    if len(vcf[0]) > 8:
-      vcf[0][8] = format_description
-    if malformed > 0:
-      # report number of malformed lines
-      print(f"There were {malformed} malformed entries", file=sys.stderr)
+  # after parsing file, set 8th element of 1st parsed row as info_description...   
+  vcf[0][7] = info_description
+  if len(vcf[0]) > 8:
+    vcf[0][8] = format_description
+  if malformed > 0:
+    # report number of malformed lines
+    print(f"There were {malformed} malformed entries", file=sys.stderr)
   return vcf
 
 if __name__ == "__main__":
@@ -127,4 +129,3 @@ if __name__ == "__main__":
   for i in range(10):
     # print just the first 10 rows of output, because vcf files can be giant!
     print(vcf[i])
-
