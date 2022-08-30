5f16ede8d8e653ecdc7183aed025abe2965fa9cb
Christina <1004parky@gmail.com>
2022-08-30 15:33:16 -0400

    Exercise 1

---
+++ day2-homework/vcfParser.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+import sys
+
+def parse_vcf(fname):
+  '''
+  Parse vcf files, return data as list of fields
+  
+  Args:
+    fname (str): Name of file 
+  Returns:
+    vcf (list): data from file as a list of lines, each of which are a list of fields
+  '''
+
+  vcf = []
+  info_description = {}
+  info_type = {}
+  format_description = {}
+  type_map = {
+    "Float": float,
+    "Integer": int,
+    "String": str
+    }
+  malformed = 0
+
+  # Check input
+  try:
+    f = open(fname)
+  except:
+    raise FileNotFoundError(f"{fname} does not appear to exist", file=sys.stderr)
+
+  for line in f: 
+    if line.startswith("#"):
+      # Parse header lines; contain valuable information
+      try:
+        if line.startswith("##FORMAT"):
+          # parse format info line, associate description with ID
+          fields = line.split("=<")[1].rstrip(">\r\n") + ","
+          i = 0
+          start = 0
+          in_string = False
+          while i < len(fields):
+            if fields[i] == "," and not in_string:
+              name, value = fields[start:i].split('=')
+              if name == "ID":
+                ID = value
+              elif name == "Description":
+                desc = value
+              start = i + 1
+            elif fields[i] == '"':
+              in_string = not in_string
+            i += 1
+          format_description[ID] = desc.strip('"')
+        elif line.startswith("##INFO"):
+          # parse info info line, associate description with ID
+          fields = line.split("=<")[1].rstrip(">\r\n") + ","
+          i = 0
+          start = 0
+          in_string = False
+          while i < len(fields):
+            if fields[i] == "," and not in_string:
+              name, value = fields[start:i].split('=')
+              if name == "ID":
+                  ID = value
+              elif name == "Description":
+                  desc = value
+              elif name == "Type":
+                  Type = value
+              start = i + 1
+            elif fields[i] == '"':
+              in_string = not in_string
+            i += 1
+          info_description[ID] = desc.strip('"')
+          info_type[ID] = Type
+        elif line.startswith('#CHROM'):
+          # parse column line. 
+          fields = line.lstrip("#").rstrip().split("\t")
+          vcf.append(fields)
+      except:
+        raise RuntimeError("Malformed header")
+    else:
+      # Data lines
+      # 8 fixed fields
+      # CHROM | POS | ID | REF | ALT | QUAL | FILTER | INFO | (other genotype fields)
+      # all rows must have the same columns
+      try:
+        fields = line.rstrip().split("\t")
+        fields[1] = int(fields[1]) # pos
+        if fields[5] != ".":
+          fields[5] = float(fields[5]) # qual 
+        info = {}
+        for entry in fields[7].split(";"): # info 
+          # for every data value in info
+          temp = entry.split("=")
+          if len(temp) == 1:
+            info[temp[0]] = None
+          else:
+            name, value = temp
+            Type = info_type[name] # capitalize Type since type is a keyword.. 
+            info[name] = type_map[Type](value)
+        fields[7] = info # dict
+        if len(fields) > 8: # genotype fields
+          fields[8] = fields[8].split(":")
+          if len(fields[8]) > 1:
+            for i in range(9, len(fields)):
+              fields[i] = fields[i].split(':') 
+          else:
+            fields[8] = fields[8][0]
+        vcf.append(fields)
+      except:
+        # Keep track of how many malformed lines
+        malformed += 1
+
+    # after parsing file, set 8th element of 1st parsed row as info_description... 
+    vcf[0][7] = info_description
+    # If there is space, set 9th element of 1st parsed row as format_description... 
+    if len(vcf[0]) > 8:
+      vcf[0][8] = format_description
+    if malformed > 0:
+      # report number of malformed lines
+      print(f"There were {malformed} malformed entries", file=sys.stderr)
+  return vcf
+
+if __name__ == "__main__":
+  fname = sys.argv[1]
+  vcf = parse_vcf(fname)
+  for i in range(10):
+    # print just the first 10 rows of output, because vcf files can be giant!
+    print(vcf[i])
+
