a2f1ce8500e8f0dce3d187ff7767517a479dc8d5
Christina <1004parky@gmail.com>
2022-08-30 20:04:01 -0400

    removed POS only option, implemented ALT matching

--- day2-homework/README.md
+++ day2-homework/README.md
@@ -12,4 +12,4 @@ I had implemented a check when creating the position to ID dictionary that check
 
 Actually, I decided to add an option for the user to specify whether they wanted to be robust and use all three data points or just use the pos and suppress any ID overwrites.
 
-
+After thinking about it more, I decided finally to always use (CHROM, POS, REF) as the keys, but also keep track of the ALTs in the dict values; doing so allowed me to check if the unlabeled set who matched the (CHROM, POS, REF) had an ALT that was listed as an ALT in the labeled dataset. In implementing this, I did decide arbitrarily to overwrite a value in the dict if the new (CHROM, POS, REF) key had at least as many ALTs as the existing key-value pair. This was not the most logical thing to do but would do for now; I think a next step could be to make nested dicts, so use (CHROM, POS, REF) as the key to another dict that pairs individual ALT options with a single ID.
--- day2-homework/label_records.py
+++ day2-homework/label_records.py
@@ -5,16 +5,13 @@ from vcfParser import parse_vcf
 
 # Usage: python label_records.py vcf_unlabeled vcf_labeled [0 or 1]
 
-def annotate(unlabeled, labels, robust=False):
+def annotate(unlabeled, labels):
   '''
   Annotate one variant dataset with the IDs from another variant dataset
 
   Args:
     unlabeled (list): Parsed output from parse_vcf, should contain metadata and data from vcf file. This is the unlabeled dataset
     labels (list): Parsed output from parse_vcf, should contain metadata and data from vcf file. Will use IDs from this dataset
-    robust (bool): Default False. If True, will map IDs based on (position, reference, alternate) tuple rather than just position. 
-                    Any overwrites (non-unique mappings) will stop the program.
-                   If False, will map IDs based only on position; any duplicate mappings will be ignored (only the first ID will be kept)
   Returns:
     labeled (list): Edited version of `unlabeled` with new labels from `labels` dataset
   '''
@@ -22,46 +19,43 @@ def annotate(unlabeled, labels, robust=False):
   # Data line format (required entries): 
   # CHROM | POS | ID | REF | ALT | QUAL | FILTER | INFO | (other genotype fields)
 
-  if robust:
-    print('Using POS, REF, and ALT to match IDs')
-  else:
-    print('Using just POS to match IDs')
+  print('Using CHROM, POS, and REF, and ALT to match IDs')
+  # alt can be a comma-separated list. the alt in unlabeled should be one of the alt's from the labels
+  # Will not use alt in dict key but will make it a value as well so that
+  # during the replace id part of the code I can check 
 
   reading_data = False
   id_dict = {}
   for d in labels:
-    if reading_data:
-      if robust:
-        # use pos, ref, alt
-        if (d[1], d[3], d[4]) in id_dict and id_dict[(d[1], d[3], d[4])] != d[2]:
-          # if pos key exists, but maps to different ID, print warning
-          raise Exception('Labeled dataset contained duplicate (POS, REF, ALT) value with different ID values.')
-        id_dict[(d[1], d[3], d[4])] = d[2]
-      else: # not robust
-        if d[1] in id_dict: 
-          # if pos key exists, ignore 
-          continue 
-        id_dict[d[1]] = d[2]
-    elif d[0] == 'CHROM': 
-      # Line with column names immediately precedes data rows 
-      reading_data = True 
+    if d[0] == 'CHROM': continue # skip header line
+    # use chrom, pos, ref
+    key = (d[0], d[1], d[3])
+    if key in id_dict and id_dict[key] != (d[4], d[2]):
+      # if pos key exists, but maps to different ID, it means same ref, different alts
+      # Keep the one with more alts; this is arbitrary
+      if d[4].count(',') < id_dict[key][0].count(','):
+        # Old line has more alts, ignore, otherwise will overwrite
+        continue
+    id_dict[key] = (d[4], d[2]) # ALT, ID
 
-  ### Replace IDs in unlabeled based on id_dict
+  ### Replace IDs in unlabeled based on id_dict -- check ALTs here
   # Track number of records without corresponding ID
   orphans = 0 
   for record in unlabeled:
+    if record[0] == 'CHROM': continue # skip header line
     try:
-      # replace ID based on position or (POS, REF, ALT)
-      if robust:
-        record[2] = id_dict[(record[1], record[3], record[4])]
-      else:
-        record[2] = id_dict[record[1]]
+      # replace ID based on (CHROM, POS, REF)
+      match = id_dict[(record[0], record[1], record[3])]
+      if record[4] in match[0].split(','):
+        # if record ALT is one of ALTs from labels
+        # Assumes that the unlabeled ALTs are only ever a single allele
+        record[2] = match[1]
+      else: raise Exception
     except:
       # that position doesn't exist
       orphans += 1
 
   print(f'{orphans} records did not have matching IDs in second file.')
-
   return unlabeled # It's actually labeled now  
 
 if __name__ == "__main__":
@@ -71,10 +65,6 @@ if __name__ == "__main__":
   vcf_unlabeled = parse_vcf(unlabeled)
   vcf_labeled = parse_vcf(labeled)
 
-  try:
-    # User specifies robust or not 
-    annotate(vcf_unlabeled, vcf_labeled, bool(sys.argv[3]))
-  except:
-    annotate(vcf_unlabeled, vcf_labeled)
+  annotate(vcf_unlabeled, vcf_labeled)
     
 
