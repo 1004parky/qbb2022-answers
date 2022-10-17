29b612886a36fe8be26f076eb5a4e210a5cad0ac
Christina <1004parky@gmail.com>
2022-10-17 15:26:20 -0400

    I think I completed assignment; not too sure about difference matrices, though

--- week6-homework/load_data.py
+++ week6-homework/load_data.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np 
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-from scipy import ndimage
+import matplotlib.transforms as mtransforms
 
 # analysis/hic_results/matrix/ddCTCF/iced/6400/ddCTCF_ontarget_6400_iced.matrix
 # analysis/hic_results/matrix/ddCTCF/iced/6400/dCTCF_ontarget_6400_iced.matrix
@@ -26,7 +26,7 @@ def get_bins(bin_fname, chrom, start, end):
 def process_data(fname, start, end):
     data = np.loadtxt(fname, dtype=np.dtype([
         ('F1', int), ('F2', int), ('score', float)]))
-    data = data[np.where((data['F1'] >= start) & (data['F2'] <= end))]
+    data = data[np.where((data['F1'] >= start) & (data['F2'] < end))]
     data['score'] = np.log(data['score']) 
     data['score'] = data['score'] - np.min(data['score'])
 
@@ -73,7 +73,6 @@ if __name__ == "__main__":
     start, end = get_bins(bin_fname, b'chr15', 11170245, 12070245)
 
     dim = end - start
-
     dd_data = process_data(dd_matrix, start, end)
     d_data = process_data(d_matrix, start, end)
 
@@ -86,7 +85,7 @@ if __name__ == "__main__":
 
     axs[0].imshow(-1*dd_mat, cmap='magma', vmin=-vmax, vmax=0)
     axs[1].imshow(-1*d_mat, cmap='magma', vmin=-vmax, vmax=0)
-    axs[2].imshow((remove_dd_bg(smooth_matrix(dd_mat))-remove_dd_bg(smooth_matrix(d_mat))), cmap='seismic', norm=colors.CenteredNorm())
+    axs[2].imshow((remove_dd_bg(smooth_matrix(d_mat))-remove_dd_bg(smooth_matrix(dd_mat))), cmap='seismic', norm=colors.CenteredNorm())
     
     axs[0].axis('off')
     axs[1].axis('off')
@@ -94,84 +93,80 @@ if __name__ == "__main__":
 
     axs[0].set_title('ddCTCF')
     axs[1].set_title('dCTCF')
-    axs[2].set_title('difference map')
+    axs[2].set_title('dCTCF - ddCTCF')
 
     plt.suptitle('HiC plot for chr15:11170245-12070245')
     plt.tight_layout()
     plt.savefig('hiC.png')
-    plt.show()
 
-    ### FULL DATA SETS ###
+    ### FULL DATA SETS ### Still looking at chr15:11170245-12070245
     dd_full = 'matrix/ddCTCF_full.6400.matrix'
     d_full = 'matrix/dCTCF_full.6400.matrix'
 
-    start, end = get_bins('matrix/6400_bins.bed', b'chr15', 10400000, 13400000)
-    # start = 54878
-    # end = 54951
+    start, end = get_bins('matrix/6400_bins.bed', b'chr15', 11170245, 12070245)
     dd_all = process_data(dd_full, start, end) 
     d_all = process_data(d_full, start, end) 
-
+    
     dim = end - start
-
-    dd_mat = populate_matrix(dd_all, dim)
+    
     d_mat = populate_matrix(d_all, dim)
-
+    dd_mat = populate_matrix(dd_all, dim)
+    
     fig, axs = plt.subplots(1,3)
 
     vmax = np.max([np.max(dd_mat), np.max(d_mat)])
 
     axs[0].imshow(-1*dd_mat, cmap='magma', vmin=-vmax, vmax=0)
     axs[1].imshow(-1*d_mat, cmap='magma', vmin=-vmax, vmax=0)
-    axs[2].imshow((remove_dd_bg(smooth_matrix(dd_mat))-remove_dd_bg(smooth_matrix(d_mat))), cmap='seismic', norm=colors.CenteredNorm())
+    axs[2].imshow((remove_dd_bg(smooth_matrix(d_mat))-remove_dd_bg(smooth_matrix(dd_mat))), cmap='seismic', norm=colors.CenteredNorm())
     axs[0].set_title('ddCTCF')
     axs[1].set_title('dCTCF')
-    axs[2].set_title('difference map')
+    axs[2].set_title('dCTCF - ddCTCF')
 
     axs[0].axis('off')
     axs[1].axis('off')
     axs[2].axis('off')
 
-    plt.suptitle('HiC plot for chr15:10400000-13400000')
+    plt.suptitle('HiC plot for full 6400 data set, chr15:11170245-12070245')
     plt.tight_layout()
     plt.savefig('hiC_full.png')
-    plt.show()
 
-    # Now insulation scores
-    dd_insul = []
+    ### INSULATION SCORES ###
+    # Now insulation scores for 10400000-13400000
+    d_full = 'matrix/dCTCF_full.40000.matrix'
+
+    # start, end = get_bins('matrix/40000_bins.bed', b'chr15', 10400000, 13400000)
+    # print(start,end) # Note, the end I find is 54954 which is not what the HW listed (54951)
+    start = 54878
+    end = 54951
+    d_all = process_data(d_full, start, end) 
+
+    dim = end - start
+
+    d_mat = populate_matrix(d_all, dim)
     d_insul = []
-    for i in range(dd_mat.shape[0]-10):
-        dd_insul.append(np.mean(dd_mat[(i - 5):(i+5), (i-5):(i + 5)]))
-    
-    for i in range(d_mat.shape[0]-10):
-        d_insul.append(np.mean(d_mat[(i - 5):(i+5), (i-5):(i + 5)]))
 
-    assert(np.sum(np.array(dd_insul) != np.array(d_insul)) > 0)
-    print(d_insul)
-    fig, ax = plt.subplots(2, 2, gridspec_kw={'height_ratios': [3, 1]}, figsize=(10,6.25))
-    ax[0,0].axis('off')
-    rot = ndimage.rotate(d_mat, 45)
-    ax[0,0].imshow(-1*rot[:int(len(rot)/2)], cmap='magma', vmin=-vmax, vmax=0)
-    ax[1,0].plot(np.arange(len(d_insul)), d_insul)
+    for i in range(d_mat.shape[0]):
+        d_insul.append(np.mean(d_mat[(i - 5):i, i:(i + 5)]))
+
+    fig, ax = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]}, figsize=(5,6.25))
+    ax[0].axis('off')
+    im = ax[0].imshow(-1*d_mat, cmap='magma', vmin=-np.max(d_mat), vmax=0)
+
+    # Rotate so that diagonal is on the x axis
+    trans_data = mtransforms.Affine2D().rotate_deg(-45).translate(0,19) + ax[0].transData
+    im.set_transform(trans_data)
+    ax[0].set_xlim(-1, 103)
+    ax[0].set_ylim(19, 75)
+
+    ax[1].plot(np.arange(len(d_insul)), d_insul)
     plt.margins(x=0)
-    ax[1,0].set_xticks([0, len(d_insul)-1])
-    ax[1,0].set_xticklabels([10400000, 13400000])
+    ax[1].set_xticks([0, len(d_insul)-1])
+    ax[1].set_xticklabels([10400000, 13400000])
 
-    ax[0,1].axis('off')
-    rot = ndimage.rotate(dd_mat, 45)
-    ax[0,1].imshow(-1*rot[:int(len(rot)/2)], cmap='magma', vmin=-vmax, vmax=0)
-    ax[1,1].plot(np.arange(len(dd_insul)), dd_insul)
-    plt.margins(x=0)
-    ax[1,1].set_xticks([0, len(dd_insul)-1])
-    ax[1,1].set_xticklabels([10400000, 13400000])
+    ax[1].set_ylabel('Insulation score')
 
-    ax[0,0].set_title('dCTCF')
-    ax[0,1].set_title('ddCTCF')
-
-    ax[1,0].set_ylabel('Insulation score')
-    ax[1,1].set_ylabel('Insulation score')
-
-    ax[1,0].set_xlabel('Position')
-    ax[1,1].set_xlabel('Position')
+    ax[1].set_xlabel('Position')
 
     plt.subplots_adjust(left=0.15,
                     bottom=0.1,
@@ -179,10 +174,9 @@ if __name__ == "__main__":
                     top=1.0,
                     wspace=0.4,
                     hspace=0.0)
-    plt.tight_layout()
     plt.suptitle('HiC and insulation scores')
+    plt.tight_layout()
     plt.savefig('hic_insulation.png')
-    plt.show()
 
 
     
